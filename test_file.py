54126cc8cec0cbbfc95404e52fd7290480cdf7c5
Mihail5032 <mihailsiskanov5101@gmail.com>
2026-03-20 17:57:13 +0300

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -1,8 +1,8 @@
 # --- Ключи для PropertiesHolder ---
 s3.endpoint=http://minio.minio.svc.cluster.local:9000
 s3.path.style.access=true
-s3.access.key.id=PeY5qkotF86JtHWbkjKe
-s3.secret.access.key=xIGISyJTDSmhjhwmDeTe0bCm6jCFjGRPcrN0C2JY
+s3.access.key.id=
+s3.secret.access.key=
 connection.ssl.enabled=false
 s3.impl=org.apache.hadoop.fs.s3a.S3AFileSystem
 io.impl=org.apache.iceberg.aws.s3.S3FileIO
