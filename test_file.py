ae8e860e1871eefa689c84709ce42b8340101746
Mihail5032 <mihailsiskanov5101@gmail.com>
2026-03-20 17:56:49 +0300

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -1,1 +1,12 @@
-00c4b5bd3e60fd0287e7
+# --- Ключи для PropertiesHolder ---
+s3.endpoint=http://minio.minio.svc.cluster.local:9000
+s3.path.style.access=true
+s3.access.key.id=PeY5qkotF86JtHWbkjKe
+s3.secret.access.key=xIGISyJTDSmhjhwmDeTe0bCm6jCFjGRPcrN0C2JY
+connection.ssl.enabled=false
+s3.impl=org.apache.hadoop.fs.s3a.S3AFileSystem
+io.impl=org.apache.iceberg.aws.s3.S3FileIO
+s3.aws.credentials.provider=org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider
+client.region=endpoint
+impl.disable.cache=true
+
