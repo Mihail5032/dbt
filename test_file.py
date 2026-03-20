14f767722e9e446c675831a5df6f8b7610e768eb
Mihail5032 <mihailsiskanov5101@gmail.com>
2026-03-20 18:58:36 +0300

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -1,23 +1,8 @@
--- Пример 1: ищем PDDATE в RAW
-SELECT fieldname, fieldvalue, retailsequencenumber
-FROM ice_baby.raw_table.raw_bplineitemextensio
-WHERE transactionsequencenumber = '100000000009451237'
-AND retailstoreid = '5695'
-AND fieldname IN ('PDDATE', 'DATAMATRIX')
-AND businessdaydate = date '2026-03-19';
-
--- Пример 2: ищем PDDATE в RAW
-SELECT fieldname, fieldvalue, retailsequencenumber
-FROM ice_baby.raw_table.raw_bplineitemextensio
-WHERE transactionsequencenumber = '100000000005294220'
-AND retailstoreid = '2465'
-AND fieldname IN ('PDDATE', 'DATAMATRIX')
-AND businessdaydate = date '2026-03-19';
-
--- Пример 3: ищем PDDATE в RAW
-SELECT fieldname, fieldvalue, retailsequencenumber
-FROM ice_baby.raw_table.raw_bplineitemextensio
-WHERE transactionsequencenumber = '100000000009013632'
-AND retailstoreid = '7252'
-AND fieldname IN ('PDDATE', 'DATAMATRIX')
-AND businessdaydate = date '2026-03-19';
+ice_baby.core_flow_ing_raw.raw_bptransactextensio
+ice_baby.core_flow_ing_raw.raw_bptransactdiscext
+raw_bpretaillineitem переименовать
+ice_baby.core_flow_ing_raw.raw_bplineitemextensio
+ice_baby.core_flow_ing_raw.raw_bplineitemdiscount
+ice_baby.core_flow_ing_raw.raw_bplineitemdiscext
+ice_baby.core_flow_ing_raw.raw_bptender переименовать
+ice_baby.core_flow_ing_raw.raw_bptenderextensions
