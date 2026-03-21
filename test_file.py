f35a5d3e78a2bc5d02fefb244f8a0a0c79c19e91
Mihail5032 <mihailsiskanov5101@gmail.com>
2026-03-21 12:16:02 +0300

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -1,11 +1,1 @@
--- Добавить is_aligned_tran в таблицы где её нет
-ALTER TABLE ice_baby.core_flow_ing_raw.raw_bptransactextensio ADD COLUMN is_aligned_tran BOOLEAN;
-ALTER TABLE ice_baby.core_flow_ing_raw.raw_bptransactdiscext ADD COLUMN is_aligned_tran BOOLEAN;
-ALTER TABLE ice_baby.core_flow_ing_raw.raw_bplineitemextensio ADD COLUMN is_aligned_tran BOOLEAN;
-ALTER TABLE ice_baby.core_flow_ing_raw.raw_bplineitemdiscount ADD COLUMN is_aligned_tran BOOLEAN;
-ALTER TABLE ice_baby.core_flow_ing_raw.raw_bplineitemdiscext ADD COLUMN is_aligned_tran BOOLEAN;
-ALTER TABLE ice_baby.core_flow_ing_raw.raw_bptenderextensions ADD COLUMN is_aligned_tran BOOLEAN;
-
--- Переименовать is_corr_receipt → is_aligned_tran
-ALTER TABLE ice_baby.core_flow_ing_raw.raw_bpretaillineitem RENAME COLUMN is_corr_receipt TO is_aligned_tran;
-ALTER TABLE ice_baby.core_flow_ing_raw.raw_bptender RENAME COLUMN is_corr_receipt TO is_aligned_tran;
+s3a://flink-operator-data/flink-checkpoints-dev/4e090dd098c291b9becbf533ebb7e4e6/chk-2855
